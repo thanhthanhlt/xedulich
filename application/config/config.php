c7f49adfdbfd6fd4cc52c5cefddf84acec7facd1
Thanh Thanh <thanhthanhspk36@gmail.com>
2014-02-11 20:13:19 +0700

    co so du lieu moi nhat

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:82/xedulich/';
+$config['base_url']	= 'http://localhost/xedulich/';
 
 /*
 |--------------------------------------------------------------------------
