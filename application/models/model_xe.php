b3f8f09e67813f849163aeac8abda47820c12945
trungnghia112 <trungnghia112@gmail.com>
2014-02-18 09:40:10 +0700

    Revert "update db"
    This reverts commit 6908429ed3ca2ebfabb998ef643ff646a67fe2a3.

--- application/models/model_xe.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Model_xe extends CI_Model{
-	public function get_chitietxe ($id){
-		$query = $this->db->get_where('chitietxe', array ('IDchitietxe' => $id));
-		return $query -> result();		
-		} 
-	public function get_loaichothue	
-}
